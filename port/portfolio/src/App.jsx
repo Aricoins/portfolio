c4122a3309ba7484f52a47ffdf534435b1ee716e
Ariel G Rogel <95644790+Aricoins@users.noreply.github.com>
2024-01-08 16:34:53 -0300

    Update App.jsx

--- port/portfolio/src/App.jsx
+++ port/portfolio/src/App.jsx
@@ -50,7 +50,7 @@ function App() {
       <Router>
       <img  data-aos="fade-up" style={{width: "100%", marginTop: "3%"}} src={head} alt="head" />
       <Content> 
-      <Img2 data-aos="fade-right" src="https://camo.githubusercontent.com/c1dcb74cc1c1835b1d716f5051499a2814c683c806b15f04b0eba492863703e9/68747470733a2f2f63646e2e6472696262626c652e636f6d2f75736572732f3733303730332f73637265656e73686f74732f363538313234332f6176656e746f2e676966" alt="devimg" />
+      <Img2 data-aos="fade-right" src="https://giphy.com/gifs/dommespace-domme-space-programador-qgQUggAC3Pfv687qPC" alt="devimg" />
       <About />
 
       </Content>
