e76d9c5b7f82de66e78e0c098faa539294af28d9
Aricoins <arielgarcia79@gmail.com>
2024-08-21 13:33:58 -0300

    foot

--- port/portfolio/src/Components/Foot.jsx
+++ port/portfolio/src/Components/Foot.jsx
@@ -1,90 +1,62 @@
-// src/Components/Foot.js
-
-import styled, {keyframes} from 'styled-components';
+"use client"
+import styled, { keyframes } from 'styled-components';
 import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
 import lat from '../../src/assets/lat.png';
-import latw from '../../src/assets/latw.png'
-
+import latw from '../../src/assets/latw.png';
 
 const shine = keyframes`
-  0% {
+  0%, 15%, 60%, 100% {
     font-size: 1rem;
   }
-  15% {
-    font-size: 1rem;
-  }
-  30% {
+  30%, 45% {
     font-size: 1.1rem;
   }
-  45% {
-    font-size: 1.1rem;
-  }
-  60% {
-    font-size: 1 rem;
-  }
-  100% {
-    font-size: 1rem;
-  }`
+`;
 
-function scrollY(e) {
+const scrollY = (e) => {
   e.preventDefault();
   window.scroll(0, 0);
-}
+};
 
 const Foot = ({ currentColor, theme }) => {
-
-
-
   const Corazon = styled.p`
-  animation: ${shine} 1s infinite;
-  cursor: pointer;
-  text-align: center;
-  justify-content: center;
-  align-self: center
-  
-  ` 
-  
-  
+    animation: ${shine} 1s infinite;
+    cursor: pointer;
+    text-align: center;
+    margin: 0;
+  `;
+
   const Footer = styled.footer`
     background-color: ${({ currentColor }) => currentColor.primero};
     color: ${({ currentColor }) => currentColor.cuarto};
-  
-    justify-content: space-around;
+    display: flex;
+    flex-direction: column;
     align-items: center;
     padding: 1rem;
     width: 90%;
     margin: auto;
     border-top: ${({ currentColor }) => `2px solid ${currentColor.quinto}`};
-    `;
-  
+    position: relative;
+  `;
+
   const SocialLinks = styled.div`
     display: flex;
-    justify-content: space-around;
-    width: 30%;
+    gap: 1rem;
+    margin: 1rem 0;
   `;
-  
-  
-
 
   return (
     <Footer currentColor={currentColor}>
-      <div  style={{
-  fontSize: "small", 
-  margin: "10%",   
-  display: "grid",
-  gridGap: "10px",
-  gridTemplateColumns: "repeat(4, [col] 100px )" ,
-  gridTemplateRows: "repeat(3, [row] auto  )",
-  backgroundColor: "#fff",
-  color: "#444"}}>
-<p width="100px" style= {{ gridColumn: "col / span 2",
-  gridRow: "row", display: "flex", justifyContent: "center"}}> Todos los derechos reservados | 2024 <br /> 
-<a href="https://github.com/Aricoins/portfolio" 
-style={{fontSize: "x-small"}}> Repositorio Open Source </a> </p>
-</div>
-<div  >
-   <SocialLinks style={{ gridColumn: "col  / span 2",
-    gridRow: "row 2"}}  >
+      <div style={{ textAlign: 'center', marginBottom: '1rem' }}>
+        <p style={{ margin: 0 }}>
+          Todos los derechos reservados | 2024 <br />
+          <a href="https://github.com/Aricoins/portfolio" style={{ fontSize: 'small' }}>
+            Repositorio Open Source
+          </a>
+        </p>
+      </div>
+
+      <SocialLinks>
         <a href="https://github.com/Aricoins" target="_blank" rel="noopener noreferrer">
           <FaGithub size={30} />
         </a>
@@ -95,19 +67,17 @@ style={{fontSize: "x-small"}}> Repositorio Open Source </a> </p>
           <FaTwitter size={30} />
         </a>
       </SocialLinks>
+      <img src={theme === 'light' ? lat : latw} alt="Logo" style={{ width: '80px', marginTop: "10%" }} />
+            <div style={{ textAlign: 'center', fontSize: "xx-small", margin: "0.5rem" }}>
+        <p style={{ margin: 0 }}>Desarrollo con latidos</p>
       </div>
-     <div style={{gridColumn: "col  / span 4",
-      gridRow: "row 3"}}>
- 
-      <img src={theme === "light" ? lat : latw} style={{width: "80px"}}/>
-       <p> desarollo con latidos </p>
-       </div>
-       <div style={{widht: "80px", height: "80px", position: "absolute", marginLeft: "43%" }}>
-       <Corazon onClick={scrollY} > ❤️ </Corazon> 
-       </div>
-       </Footer>
+      <div style={{ position: 'absolute', bottom: '0.1rem', left: '50%', transform: 'translateX(-50%)' }}>
+        <Corazon onClick={scrollY}>❤️</Corazon>
+      </div>
+
+     
+    </Footer>
   );
 };
-
 
 export default Foot;
