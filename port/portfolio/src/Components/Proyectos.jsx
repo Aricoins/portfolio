c8d74a87a2f8e2c8fefa8c17138d6c877c1ceba2
Aricoins <arielgarcia79@gmail.com>
2024-09-25 22:04:06 -0300

    rediseño proyectos

--- port/portfolio/src/Components/Proyectos.jsx
+++ port/portfolio/src/Components/Proyectos.jsx
@@ -1,4 +1,3 @@
-
 import styled from "styled-components";
 import AOS from "aos";
 import "aos/dist/aos.css";
@@ -11,7 +10,7 @@ import { useEffect } from "react";
 
 const Contenedor = styled.div`
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  grid-template-columns: repeat(3, 1fr);
   font-family: audiowide-regular;
   margin: 0%;
   justify-content: center;
@@ -20,11 +19,12 @@ const Contenedor = styled.div`
   color: ${(props) => props.currentColor.cuarto};
   @media (max-width: 800px) {
     flex-direction: column;
-    grid-template-columns: repeat(1, 1fr);
+    grid-template-columns: repeat(2, 1fr);
   }
 
   @media (max-width: 600px) {
     flex-direction: column;
+    grid-template-columns: repeat(1, 1fr);
   }
 `;
 
@@ -74,171 +74,274 @@ const Proyectos = ({ currentColor }) => {
 
   return (
     <Contenedor currentColor={currentColor}>
-      <a href="https://dibiase.net/"  rel="noreferrer" target="_blank">
+      <Card
+        currentColor={currentColor}
+        data-aos="flip-left"
+        data-aos-easing="ease-out-cubic"
+        data-aos-duration="2000"
+      >
+        <a href="https://dibiase.net/" rel="noreferrer" target="_blank">
+          <h4
+            style={{
+              fontFamily: "Verdana",
+              fontSize: "1.5em",
+              marginBottom: "10px",
+            }}
+          >
+            LawCase | Software para abogados
+          </h4>
+          
+          <p
+            style={{
+              fontFamily: "Verdana",
+              fontSize: "12px",
+              marginBottom: "15px",
+            }}
+          >
+            Plataforma integral para la gestión de casos, plazos y documentos
+            legales. Eficiencia en la administración de casos, organización de
+            documentos, gestión de clientes y programación de eventos.
+          </p>
+        </a>
+
+        <video
+          style={{ width: "100%", borderRadius: "5%", marginTop: "10%" }}
+          src="/lawvideo.mp4"
+          autoPlay
+          loop
+          muted
+        />
+        <a
+          href="https://https://s17-09-n-node-react-2.onrender.com/"
+          rel="noreferrer"
+          target="_blank"
+        >
+              <ul style={{ fontFamily: "Verdana", marginTop: "15px" }}>
+            <li>Autenticación con token refresh.</li>
+            <li>Gestión multimedia con Cloudinary.</li>
+            <li>Manejo de datos con PostgreSQL.</li>
+            <li>Estilos con MaterialUI y Tailwind.</li>
+         <li>   Desarrollado con TypeScript, React, Vite, Tailwind, MUI, Express,
+            Swagger, Prisma, MongoDB y JWT. Implementado con metodología ágil.
+            </li>
+            </ul>
+            </a>
+      </Card>
+
+      <a href="https://dibiase.net/" rel="noreferrer" target="_blank">
         <Card
           currentColor={currentColor}
           data-aos="flip-left"
           data-aos-easing="ease-out-cubic"
           data-aos-duration="2000"
         >
-          <h4> DiBiase.net </h4>
-          <b
+          <h4
             style={{
-              fontFamily: "audiowide",
-              fontSize: "small",
-              widht: "100%",
-              color: `${currentColor.quinto}`,
-              padding: "10%",
-              marginBottom: "30px",
+              fontFamily: "Verdana",
+              fontSize: "1.5em",
+              marginBottom: "10px",
             }}
           >
-            {" "}
-            Apliación web de Gestión Documental
-          </b>
+            DiBiase Net
+          </h4>
+          <p style={{ fontFamily: "Verdana", fontSize: "12px" }}>
+            WebApp para la gestión y visualización de colecciones de libros y
+            documentos archivísticos. Focalizada en la despapelización y mejora
+            de tiempos de búsqueda.
+          </p>
           <img
             src={biblio}
             alt="imagen"
             style={{ borderRadius: "2%", marginTop: "10%" }}
           />
-          <ul>
-            <li>
-              NextJS: Framework React para la creación de aplicaciones web.
-            </li>
-            <li>Clerk: Integración de autenticación y gestión de usuarios.</li>
-            <li>Compatibilidad de Tailwind CSS con PostCSS 7.</li>
-            <li>Gestión y optimización de imágenes y vídeos.</li>
-            <li>Librería oficial para acceder a las APIs de Google.</li>
-            <li>
-              pg-promise: Interfaz de Promesas para la base de datos PostgreSQL.
-            </li>
-            <li>styled-components: Librería para estilos CSS en JS.</li>
-            <li>sweetalert2: Alertas modales.</li>
-            <li>xlsx: Lectura y escritura de archivos Excel.</li>
+          <ul style={{ fontFamily: "Verdana", marginTop: "15px" }}>
+            <li>Autenticación con Clerk.</li>
+            <li>Gestión multimedia con Cloudinary.</li>
+            <li>Manejo de datos con PostgreSQL.</li>
+            <li>Next.js 14 on Vercel.</li>
+            <li>Estilos con styled-components.</li>
+            <li>Lectura y escritura de archivos Excel con xlsx.</li>
           </ul>
         </Card>
       </a>
 
+      <Card
+        currentColor={currentColor}
+        data-aos="flip-left"
+        data-aos-easing="ease-out-cubic"
+        data-aos-duration="2000"
+      >
+        <h4
+          style={{
+            fontFamily: "Verdana",
+            fontSize: "1.8em",
+            marginBottom: "10px",
+          }}
+        >
+          Azul Lago Cooperativa
+        </h4>
+        <p
+          style={{
+            fontFamily: "Verdana",
+            fontSize: "14px",
+            marginBottom: "20px",
+          }}
+        >
+          Landing page centrada en la visualización de marcas y proyectos
+          utilizando HTML5, Bootstrap, jQuery, WowJS y animaciones responsivas
+          para mejorar la experiencia del usuario.
+        </p>
+        <video
+          style={{ width: "100%", borderRadius: "5%", marginTop: "10%" }}
+          src="https://res.cloudinary.com/dx0htqhaq/video/upload/v1727229863/cjh109sdvobrlcphfmrl.mp4"
+          autoPlay
+          loop
+          muted
+        />
+        <a
+          href="https://www.azullago.com/"
+          rel="noreferrer"
+          target="_blank"
+          style={{ display: "block", textAlign: "center", marginTop: "20px" }}
+        >
+          <button
+            style={{
+              padding: "10px 20px",
+              backgroundColor: `${currentColor.quinto}`,
+              color: "#fff",
+              border: "none",
+              borderRadius: "5px",
+              fontSize: "1em",
+              cursor: "pointer",
+            }}
+          >
+            Visita la Landing Page
+          </button>
+        </a>
+        <ul
+          style={{
+            fontFamily: "Verdana",
+            fontSize: "13px",
+            marginTop: "20px",
+            paddingLeft: "20px",
+          }}
+        >
+          <li>Sitio web completamente responsivo.</li>
+          <li>Animaciones integradas con WowJS.</li>
+          <li>HTML5, CSS3 y JS.</li>
+          <li>Diseño maquetado en Figma.</li>
+        </ul>
+      </Card>
 
+      <a
+        href="https://ecommerce-pf-henry-grupo7.vercel.app/"
+        rel="noreferrer"
+        target="_blank"
+      >
         <Card
           currentColor={currentColor}
           data-aos="flip-left"
           data-aos-easing="ease-out-cubic"
           data-aos-duration="2000"
         >
-          <h4> Azul Lago Cooperativa </h4>
+          <h4 style={{ fontFamily: "Verdana" }}>Codewave Central</h4>
           <b
             style={{
-              fontFamily: "audiowide",
+              fontFamily: "Verdana",
               fontSize: "small",
-              widht: "100%",
+              width: "100%",
               color: `${currentColor.quinto}`,
               padding: "10%",
               marginBottom: "30px",
             }}
           >
-            Página web de difusión y desarrollo de marcas{" "}
-          </b>
-          <video
-  style={{ width: "100%" }}
-  src="https://res.cloudinary.com/dx0htqhaq/video/upload/v1727229863/cjh109sdvobrlcphfmrl.mp4"
-  autoPlay
-  loop
-  muted
-/>
-
-            
-                  <a href="https://www.azullago.com/" rel="noreferrer" target="_blank">
-          <ul>
-            <li>Sitio web responsivo</li>
-            <li>HTML5 y CSS3 válidos</li>
-            <li>Framework Bootstrap v5</li>
-            <li>Código 100% amigable para desarrolladores y SEO</li>
-            <li>Compatible con todos los navegadores web modernos</li>
-            <li>Navbar amigable para móviles y usuarios</li>
-            <li>Header elegante y responsivo con carrusel</li>
-            <li>Animaciones de desplazamiento de página</li>
-            <li>Contador de números animado</li>
-            <li>Google Map</li>
-          </ul>
-          </a>
-        </Card>
-
-
-      <a href="https://ecommerce-pf-henry-grupo7.vercel.app/" rel="noreferrer" target="_blank">
-        <Card
-          currentColor={currentColor}
-          data-aos="flip-left"
-          data-aos-easing="ease-out-cubic"
-          data-aos-duration="2000"
-        >
-          <h4> Codewave Central </h4>
-          <b
-            style={{
-              fontFamily: "audiowide",
-              fontSize: "small",
-              widht: "100%",
-              color: `${currentColor.quinto}`,
-              padding: "10%",
-              marginBottom: "30px",
-            }}
-          >
-            {" "}
             Tienda de comercio electrónico
           </b>
-
           <img src={nido} alt="imagen" />
-          <ul>
-            <li>Carpetas organizadas y claras.</li>
+          <ul style={{ fontFamily: "Verdana" }}>
             <li>TypeScript para seguridad y legibilidad.</li>
             <li>Next.js para sitios estáticos y SSR.</li>
-            <li>
-              Optimización de imágenes con `next-cloudinary & next/image`.
-            </li>
-            <li>Gestión de estado predecible con Redux.</li>
-            <li>Estilos eficientes con Tailwind CSS.</li>
-            <li>
-              Detección de problemas y estilización consistente con ESLint.
-            </li>
-            <li>Compatibilidad y eficiencia mejoradas con PostCSS.</li>
-            <li>Scripts personalizados para automatización.</li>
+            <li>Optimización de imágenes con next-cloudinary y next/image.</li>
+            <li>Gestión de estado con Redux.</li>
+            <li>Estilos con Tailwind CSS.</li>
+            <li>Automatización con scripts personalizados.</li>
           </ul>
         </Card>
       </a>
 
-      <a href="https://nuevamascota.vercel.app/" target="_blank" rel="noreferrer">
+      <a
+        href="https://nuevamascota.vercel.app/"
+        target="_blank"
+        rel="noreferrer"
+      >
         <Card
           currentColor={currentColor}
           data-aos="flip-left"
           data-aos-easing="ease-out-cubic"
           data-aos-duration="1500"
         >
-          <h4> Dogs App </h4>
+          <h4 style={{ fontFamily: "Verdana" }}>Dogs App</h4>
           <b
             style={{
-              fontFamily: "audiowide",
+              fontFamily: "Verdana",
               fontSize: "small",
-              widht: "100%",
+              width: "100%",
               color: `${currentColor.quinto}`,
               padding: "10%",
               marginBottom: "30px",
             }}
           >
-            {" "}
-            Aplicación web estándar{" "}
+            Aplicación web interactiva de adopción de mascotas. Permite a los
+            usuarios explorar una amplia variedad de razas de perros, buscar por
+            nombre, filtrar por características, ordenar alfabéticamente o por
+            peso, y crear nuevas razas de perros en la base de datos. Además, la
+            aplicación implementa una API con endpoints específicos para
+            gestionar las razas y temperamentos, proporcionando una experiencia
+            de usuario fluida y responsiva.
           </b>
-
           <img src={dogs} alt="imagen" />
-          <ul>
-            <li>Diseño UX/UI planificado y consistente en todas las rutas.</li>
+          <ul style={{ fontFamily: "Verdana" }}>
+            <li>Diseño UX/UI planificado.</li>
             <li>Filtros y ordenamientos combinados.</li>
-            <li>Formulario controlado. Envío condicional.</li>
-            <li>Manejo de diferentes estados de Redux.</li>
-            <li>Paginación, despliegue y pruebas.</li>
-            <li>Código modularizado con responsabilidades bien definidas.</li>
-            <li>Seguir buenas prácticas de API Rest.</li>
-            <li>
-              BD normalizada, validaciones y restricciones en los modelos.
-            </li>
+            <li>Formulario controlado y validaciones.</li>
+            <li>Gestión de estado con Redux.</li>
+            <li>Paginación y pruebas unitarias.</li>
+          </ul>
+        </Card>
+      </a>
+      <a
+        href="https://github.com/Aricoins/APIRest"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Card
+          currentColor={currentColor}
+          data-aos="flip-left"
+          data-aos-easing="ease-out-cubic"
+          data-aos-duration="1500"
+        >
+          <h4 style={{ fontFamily: "Verdana" }}>WeCode API</h4>
+          <b
+            style={{
+              fontFamily: "Verdana",
+              fontSize: "small",
+              width: "100%",
+              color: `${currentColor.quinto}`,
+              padding: "10%",
+              marginBottom: "30px",
+            }}
+          >
+            API REST para la red social WeCode
+          </b>
+          <img src="/apirest.jpg" alt="imagen" />
+          <ul style={{ fontFamily: "Verdana" }}>
+            <li>Autenticación y gestión de usuarios con Clerk.</li>
+            <li>CRUD completo de usuarios, posts y comentarios.</li>
+            <li>Relaciones entre seguidores.</li>
+            <li>Soporte de pagos con Stripe.</li>
+            <li>Almacenamiento con AWS S3.</li>
+            <li>Paginación y búsqueda incremental en publicaciones.</li>
+            <li>Protección de rutas y roles de administración.</li>
           </ul>
         </Card>
       </a>
--- port/portfolio/src/Components/about.jsx
+++ port/portfolio/src/Components/about.jsx
@@ -1,55 +1,36 @@
 import { useEffect } from "react";
 import styled from "styled-components";
+import PropTypes from "prop-types"; // Importamos PropTypes
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { Link } from "react-router-dom";
-import  CalendlyBadge from "./calendly"
+import CalendlyBadge from "./calendly";
+
 function About({ currentColor }) {
-
-
   const Container = styled.div`
-  width: 90%;
-  margin: 5% auto;
-  padding: 2%;
-  text-align: center;
-  font-family: 'Audiowide', sans-serif;
-  color: ${currentColor.primero};
-  
-  @media (max-width: 600px) {
-    width: 100%;
+    width: 90%;
+    margin: 5% auto;
     padding: 2%;
-  }
-`;
+    text-align: center;
+    font-family: 'Audiowide', sans-serif;
+    color: ${currentColor.primero};
 
-const Title = styled.h2`
-  font-size: 2rem;
-  margin: 0;
-  color: ${currentColor.quinto};
-  text-transform: uppercase;
-  font-weight: bold;
+    @media (max-width: 600px) {
+      width: 100%;
+      padding: 2%;
+    }
+  `;
 
-  @media (max-width: 600px) {
-    font-size: 1.5rem;
-  }
-`;
+  const Text = styled.p`
+    font-size: 1.3rem;
+    color: ${currentColor.primero};
+    line-height: 1.6;
+    margin-top: 1rem;
+    font-family: Verdana;
 
-const Text = styled.p`
-  font-size: 1.3rem;
-  color: ${currentColor.primero};
-  line-height: 1.6;
-  margin-top: 1rem;
-  font-family: Verdana;
-
-  @media (max-width: 600px) {
-    font-size: 0.9rem;
-  }
-`;
-
-
-const rollContacto = () => {
-
-window.scrollTo( 0, 5000)
-}
+    @media (max-width: 600px) {
+      font-size: 0.9rem;
+    }
+  `;
 
   useEffect(() => {
     AOS.init();
@@ -57,20 +38,23 @@ window.scrollTo( 0, 5000)
 
   return (
     <Container data-aos="fade-up">
-      <Title> ¡Bienvenid@s! </Title>
       <Text>
-       Ofrecemos soluciones tecnológicas avanzadas que ayudan a transformar ideas en realidades digitales. 
-       Con enfoque en la innovación y el diseño centrado en el usuario, creamos software y aplicaciones web que aceleran el éxito de nuestros clientes. 
-     
-         </Text>
-         <div style= {{ position: "absolute", 
-     backgroundColor: "black"
-   }}>
-     <CalendlyBadge />
-    </div>
+        Ofrecemos soluciones tecnológicas avanzadas que ayudan a transformar ideas en realidades digitales. 
+        Con enfoque en la innovación y el diseño centrado en el usuario, creamos software y aplicaciones web que aceleran el éxito de nuestros clientes. 
+      </Text>
+      <div style={{ position: "absolute", backgroundColor: "black" }}>
+        <CalendlyBadge />
+      </div>
     </Container>
   );
 }
 
-export default About;
+// Definimos los PropTypes para la validación de las props
+About.propTypes = {
+  currentColor: PropTypes.shape({
+    primero: PropTypes.string.isRequired, // 'primero' es requerido
+    quinto: PropTypes.string.isRequired, // 'quinto' es requerido
+  }).isRequired,
+};
 
+export default About;
