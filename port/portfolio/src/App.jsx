49cd2f47e502905a22eb53deaafc398efba913e0
Ariel Rogel <95644790+Aricoins@users.noreply.github.com>
2025-01-09 11:28:51 -0300

    Update App.jsx

--- port/portfolio/src/App.jsx
+++ port/portfolio/src/App.jsx
@@ -201,6 +201,14 @@ function App() {
               <Img2 data-aos="fade-up"  src={theme === "light" ? lat : latw} style={{ width: '30%' }} alt='devimg' />
          
             </Div>
+<iframe
+  src="https://drive.google.com/file/d/1NZj7aDDFIQLiwHsLHsxpfQJq1U1EzxcH/preview"
+  width="100%"
+  height="500"
+  allow="autoplay"
+  style={{ border: 'none' }}
+/>
+
           <StyledDiv>
             <h2 style={{ fontFamily: "audiowide",
              fontSize: '20px',
